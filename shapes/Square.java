61adf755986e066887c1cb798e6667fbf5062ebe
Sergey Stepanyan <sergstep314@gmail.com>
2016-11-22 09:47:53 +0400

    Init

---
+++ shapes/Square.java
@@ -0,0 +1,12 @@
+package shapes;
+
+public class Square {
+
+    private int length;
+    public Square(int length)
+        {this.length=length;}
+
+    public int getArea()
+        {return length*length;}
+}
+
